360de514bc3101e61c2ce0d8e8503c344bcc4a63
IvanPribytkov <pribytkov.ivan@gmail.com>
2024-01-23 23:30:54 +0500

    Исправлено: При отсутствии счета отправителя замаскированный счет выводит не нужно.

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ def mask_card_number(card_number):
     :return: Маскированный номер карты.
     :rtype: str
     """
-    # Извлекаем первые 6 цифр и последние 4 цифры номера карты
     first_part = card_number[-16:-10]
     last_part = card_number[-4:]
-    # Маскируем остальные цифры
     masked_number = f"{first_part}******{last_part}"
     masked_number = ' '.join(masked_number[i * 4:(i + 1) * 4] for i in range(4))
     return masked_number
@@ -26,41 +24,38 @@ def mask_account_number(account_number):
     :return: Маскированный номер счета.
     :rtype: str
     """
-    # Извлекаем последние 4 цифры номера счета и добавляем '**' в начале
     masked_number = '**' + account_number[-4:]
     return masked_number
 
 def main():
-    # Чтение данных из файла
-    with open('operations.json', 'r') as file:
-        operations = json.load(file)
+    try:
+        # Чтение данных из файла
+        with open('operations.json', 'r') as file:
+            operations = json.load(file)
 
-    # Фильтрация и сортировка операций
-    # Фильтруем операции с проверкой наличия ключа 'state' и его значения 'EXECUTED'
-    executed_operations = [operation for operation in operations if operation.get('state') == 'EXECUTED']
-    # Сортировка по дате в убывающем порядке и выбор последних 5 операций
-    sorted_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)[:5]
+        # Фильтрация и сортировка операций
+        executed_operations = [operation for operation in operations if operation.get('state') == 'EXECUTED']
+        sorted_operations = sorted(executed_operations, key=lambda x: x['date'], reverse=True)[:5]
 
-    # Вывод информации о последних 5 операциях
-    for operation in sorted_operations:
-        # Извлекаем только дату из строки даты и времени
-        date = operation['date'][:10]
-        date = f"{date[-2:]}.{date[5:7]}.{date[:4]}"
-        # Извлекаем описание операции
-        description = operation['description']
-        # Маскируем номер карты или счета, если они присутствуют
-        from_account = mask_card_number(operation.get('from', ''))
-        to_account = mask_account_number(operation['to'])
-        # Извлекаем сумму и валюту операции
-        amount = operation['operationAmount']['amount']
-        currency = operation['operationAmount']['currency']['name']
+        # Вывод информации о последних 5 операциях
+        for operation in sorted_operations:
+            date = operation['date'][:10]
+            date = f"{date[-2:]}.{date[5:7]}.{date[:4]}"
+            description = operation['description']
+            from_account = mask_card_number(operation.get('from', ''))
+            to_account = mask_account_number(operation['to'])
+            amount = operation['operationAmount']['amount']
+            currency = operation['operationAmount']['currency']['name']
 
-        # Форматированный вывод информации о операции
-        print(f"Дата: {date}")
-        print(f"Описание: {description}")
-        # Выводим маскированные номера карты или счета
-        print(f"Откуда: {from_account} -> Куда: {to_account}")
-        print(f"Сумма: {amount} {currency}\n")
+            print(f"Дата: {date}")
+            print(f"Описание: {description}")
+            if operation.get('from'):
+                print(f"Откуда: {from_account}")
+            print(f"Куда: {to_account}")
+            print(f"Сумма: {amount} {currency}\n")
+
+    except FileNotFoundError:
+        print("Файл 'operations.json' не найден.")
 
 if __name__ == "__main__":
     main()
